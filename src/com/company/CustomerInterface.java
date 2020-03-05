9161f5ecb3ddc0466190f8994eec2259fea18001
Juho <jtolli@utu.fi>
2020-03-05 15:02:13 +0200

    Lisättiin luokat: Reservation, ReservationTarget, Customer

---
+++ src/com/company/CustomerInterface.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public interface CustomerInterface {
+
+    String getName();
+    void setName(String name);
+
+    Date getBirthDate();
+    void setBirthDate(Date birthDate);
+
+    ArrayList<Reservation> getAllReservations();
+    void setAllReservations(ArrayList<Reservation> reservations);
+
+    Reservation getReservation(int reservationNumber);
+    void setReservation(Reservation reservation);
+
+}
---
+++ src/com/company/ReservationTargetInterface.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public interface ReservationTargetInterface {
+    String getName();
+    void setName(String name);
+
+    String getType();
+    void setType(String type);
+
+    String getAddress();
+    void setAddress(String address);
+}
