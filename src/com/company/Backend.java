9c3bfb018764369cd562ad45d282ebc6c2c6e8f3
Juhana Kuparinen <jmkupa@utu.fi>
2020-03-05 20:59:29 +0200

    Tietokannan yksinkertainen kysely toimii...

--- src/com/company/Backend.java
+++ src/com/company/Backend.java
@@ -1,8 +1,5 @@
 package com.company;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Backend {
     private final String url = "jdbc:postgresql://localhost/test";
@@ -12,12 +9,56 @@ public class Backend {
     public Connection connect(){
         Connection conn = null;
         try {
+            //Class.forNamen tarpeellisuudesta minulla ei ole
+            //harmainta aavistusta...
+            Class.forName("org.postgresql.Driver");
+            //Yhdistetään tietokantaan edellä määriteltyjen muuttujien
+            //avulla ja käsitellään virheet mikäli niitä tulee...
             conn = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to the PostgreSQL server succesfully");
         }
         catch (SQLException e){
             System.out.println(e.getMessage());
         }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return conn;
+    }
+
+    public void statement(){
+        //Yhdistetään tietokantaan connect-metodin avulla.
+        Connection conn = this.connect();
+        Statement stmt = null;
+        try{
+            //stmt muttujaa (tyyppiä Statement) käytetään nyt kyselyjen tekemiseen.
+            stmt = conn.createStatement();
+            //Valitaan kaikki informaatio mock_data_people taulukosta.
+            //Toimii vain "test" tietokannalla, joka on olemassa vain
+            // minun koneellani.
+            ResultSet rs = stmt.executeQuery("SELECT * FROM mock_data_people;");
+            while(rs.next()){
+                //Tallennetaan nykyisen rivin sarakkeiden tiedot muuttujiin.
+                String fname = rs.getString("fname");
+                String lname = rs.getString("lname");
+                Date bdate = rs.getDate("bdate");
+                int ssn = rs.getInt("ssn");
+                //Tulostetaan kyselyn palauttamat arvot.
+                System.out.println(fname);
+                System.out.println(lname);
+                System.out.println(bdate);
+                System.out.println(ssn);
+                System.out.println("=============");
+            }
+            //En tiiä mikä on näiden virka...
+            rs.close();
+            stmt.close();
+            conn.close();
+        }
+        catch(Exception e){
+            System.out.println("Jokin meni pieleen...");
+            System.out.println(e);
+            //System.exit(0);
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	    Backend backend = new Backend();
-	    backend.connect();
+	    //Suoritetaan hard-koodattu statement metodi Backend-luokassa.
+        //Tämä heittää luultavasti koneellasi vain errorin...
+        //Tietokanta toimii tällä hetkellä vain minun koneellani :)
+	    backend.statement();
     }
 }
